4d5a1eddac0d2fdff99c3b94746257db76ac92d0
Muaaz <muaaz9911@gmail.com>
2020-02-26 14:59:59 -0800

    initial commit. api usining passport done

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    //
+
+
+public function register(request $request){
+
+    $validatedData = $request->validate([
+
+        'name' => 'required',
+        'email' => 'required|email',
+        'password' => 'required',
+        // 'c_password' => 'required|same:password',
+    ]);
+
+    $input = $request->all();
+    $input['password'] = bcrypt($input['password']);
+    $user = User::create($input);
+    $token =  $user->createToken('Mytoken')->accessToken;
+
+    return response(['message'=>'user registered sucessfully' , 'user'=>$user , 'access_token' => $token]);
+
+
+}
+
+public function login( request $request){
+
+    $validatedData = $request->validate([
+
+        
+        'email' => 'required|',
+        'password' => 'required',
+     
+    ]);
+
+
+    if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
+
+        $user = Auth::user(); 
+
+        $token =  $user->createToken('Mytoken')->accessToken;
+
+        return response([ 'message'=>'user login sucessfully' , 'user'=>$user , 'access_token' => $token]);
+    } 
+    else{ 
+        return "some error";
+    } 
+
+
+}
+
+
+
+
+}
---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    // public function __construct(){
+
+    //     $this->middleware('auth:api')->except('index','show');
+ 
+ 
+    // }
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Product::all();
+        //
+    }
+
+    
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+
+        $validatedData = $request->validate([
+
+            'name' => 'required',
+            'details' => 'required',
+        ]);
+
+        $product = Product::create($request->all());
+        return $request;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(product $product)
+    {
+        //
+
+        $p = Product::find( $product->id);
+        // return response()->json($p, 200);
+          if(is_null($p))
+        {
+            return response()->json(['message'=>'userregistered sucessfully']);
+            
+            // return  "not foullnd";
+        }
+        
+            return response()->json($p, 200);
+        
+        
+    }
+
+  
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(request $request , product $product)
+    {
+        //
+      
+        $product->update($request->all());
+        echo "update done";
+        return $product;
+           
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(product $product)
+    {
+        
+        if (is_null($product)){
+             return response()->json("not foujnd" , 404);
+        }
+        $product->delete();
+        // return response()->json("deleted" , 204);
+        echo "done";
+        //
+    }
+}
